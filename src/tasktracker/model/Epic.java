44377b3722280cf7d1ccf32a79ca32328f255c6b
Pavel <zuikin1616@gmail.com>
2025-06-05 10:24:01 +0300

    add Epic and Subtask classes extending Task with relationships

---
+++ src/tasktracker/model/Epic.java
@@ -0,0 +1,34 @@
+package tasktracker.model;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private final List<Integer> subtaskIds = new ArrayList<>();
+
+    public Epic(int id, String name, String description, Status status) {
+        super(id, name, description, status);
+    }
+
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove((Integer) subtaskId);
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+}
---
+++ src/tasktracker/model/Subtask.java
@@ -0,0 +1,25 @@
+package tasktracker.model;
+
+public class Subtask extends Task {
+    private final int epicId;
+
+    public Subtask(int id, String name, String description, Status status, int epicId) {
+        super(id, name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", epicId=" + epicId +
+                '}';
+    }
+}
